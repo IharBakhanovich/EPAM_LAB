afdeb7c54e83018e31881af2c0e4b78ae463df5c
Ihar <ihar.bakhanovich@gmail.com>
2021-10-26 14:17:34 +0300

    created entities 'User', 'Order'

---
+++ certificateshop-logic/src/main/java/com/epam/esm/model/impl/Order.java
@@ -0,0 +1,24 @@
+package com.epam.esm.model.impl;
+
+import com.epam.esm.model.DatabaseEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * The POJO that describes the {@link Order} entity in the system.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order implements DatabaseEntity {
+    private long id;
+    private User user;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime createDate;
+    private List<GiftCertificate> certificates;
+}
---
+++ certificateshop-logic/src/main/java/com/epam/esm/model/impl/User.java
@@ -0,0 +1,20 @@
+package com.epam.esm.model.impl;
+
+import com.epam.esm.model.DatabaseEntity;
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * The POJO that describes the {@link User} entity in the system.
+ */
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements DatabaseEntity {
+    private long id;
+    private String nickName;
+    private List<Order> orders;
+}
